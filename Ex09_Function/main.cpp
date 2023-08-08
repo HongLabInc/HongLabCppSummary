2ffa77e9df31b9d36232d58152fa7b10ea7b7a26
Jeong-Mo Hong <HongLab2021@gmail.com>
2023-08-07 22:35:12 -0700

    Update comments

--- Ex09_Function/main.cpp
+++ Ex09_Function/main.cpp
@@ -8,13 +8,13 @@ using namespace std;
 
 const int kMaxStr = 100; // 전역 변수 소개
 
-// 함수 (선언과 정의 분리 가능하다고 안내)
+// 함수 (선언과 정의 분리 가능)
 int Add(int a, int b)
 {
     return a + b; // 반환값 안내
 }
 
-// 반환 자료형이 지정되지 않았음
+// 반환 자료형이 지정되지 않았음 (void)
 void Add(int a, int b, int *c)
 {
     *c = a + b;
