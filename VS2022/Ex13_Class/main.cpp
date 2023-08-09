c9541b808c6f8b3b0c94b01b6693f1ffb2c2e2f8
Jeong-Mo Hong <HongLab2021@gmail.com>
2023-08-08 17:52:00 -0700

    Overwrite Ex13_Class example.

--- VS2022/Ex13_Class/main.cpp
+++ VS2022/Ex13_Class/main.cpp
@@ -7,110 +7,59 @@
 
 using namespace std;
 
-// public, private 접근 권한 안내
+// public, private 접근 권한 확인
 
-class MyString
+class MyClass
 {
 public:
-    MyString(const char *init_str) // init_str이 유효한 메모리라고 가정
-    {
-        cout << "MyString(const char *init_str)" << endl;
-
-        // 1. 글자 수 먼저 확인
-        size_ = 0;
-        while (init_str[size_] != '\0')
-        {
-            size_++; // ++ 연산자 안내
-        }
-
-        // 2. 글자 수가 0이 아니면 메모리 할당
-        if (size_ > 0)
-        {
-            str_ = new char[size_];
-        }
-
-        // 3. 복사
-        for (int i = 0; i < size_; i++)
-        {
-            str_[i] = init_str[i];
-        }
-        // memcpy() 사용 가능
-    }
-
-    ~MyString()
+    MyClass()
     {
         // 호출 시점 확인
-        cout << "Destructor" << endl;
-
-        size_ = 0;
-        if (str_)
-            delete[] str_;
+        cout << "MyClass()" << endl;
     }
 
-    void Resize(int new_size)
+    MyClass(int number) // init_str이 유효한 메모리라고 가정
     {
-        char *new_str = new char[new_size];
+        cout << "MyClass(int number)" << endl;
 
-        // memcpy() 사용 가능
-        for (int i = 0; i < (new_size < size_ ? new_size : size_); i++)
-        {
-            new_str[i] = str_[i];
-        }
+        // this pointer 소개
+        this->number_ = number;
+    }
 
-        delete[] str_;
-        str_ = new_str;
-        size_ = new_size;
+    ~MyClass()
+    {
+        // 호출 시점 확인
+        cout << "~MyClass()" << endl;
+    }
 
-        // new_str 지우면 안되요!
+    void Increment(int a)
+    {
+        number_ += a;
     }
 
     void Print()
     {
-        for (int i = 0; i < size_; i++)
-        {
-            cout << str_[i];
-        }
-        cout << endl;
-    }
-
-    void Append(MyString *app_str) // 같은 타입을 매개변수로 사용 가능
-    {
-        int old_size = size_;
-
-        // 다른 멤버 함수 호출 가능
-        // Resize(...);
-
-        // 중요한 개념
-        for (int i = old_size; i < size_; i++)
-        {
-            // TODO: 복사
-        }
+        cout << number_ << endl;
     }
 
 private:
-    int size_ = 0;        // m_size
-    char *str_ = nullptr; // m_str (여기서는 구글 스타일)
+    int number_ = 0; // 초기값
 };
 
 int main()
 {
-    MyString str1("ABCDE"); // 생성자 이용
-    MyString str2("123");
+    MyClass my_class1;
+    MyClass my_class2(123);
 
-    str1.Print();
+    my_class1.Print();
+    my_class2.Print();
 
-    str1.Append(&str2); // 주소 넣어줌
+    my_class1.Increment(1);
+    my_class1.Print();
 
-    str1.Print();
-
-    str1.Append(&str2); // 주소 넣어줌
-
-    str1.Print();
-
-    MyString str3("Hello, World!");
-
-    str3.Append(&str1);
-    str3.Print();
+    // 배열 사용 가능
+    // 포인터 사용 가능 등 안내
+    // 기본 자료형과 비교
 
     return 0;
 }
